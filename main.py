5c1a9c5f58a09dc4f0a2ba7e0701c693c059626b
samuelhellen <162449753+samuelhellen@users.noreply.github.com>
2024-08-16 11:13:11 +0300

    class

--- main.py
+++ main.py
@@ -12,3 +12,24 @@ import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, BatchNormalization 
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+
+class DataLoader:
+    def __init__(self, data_dir, emotions):
+        self.data_dir = data_dir
+        self.emotions = emotions
+        self.X = []  # List to store audio data (as numpy arrays)
+        self.y = []  # List to store corresponding labels
+
+    def load_data(self):
+        for i, emotion in enumerate(self.emotions):
+            emotion_dir = os.path.join(self.data_dir, emotion)
+            wav_files = [f for f in os.listdir(emotion_dir) if f.endswith('.wav')]
+            print(f"Number of .wav files in {emotion} folder: {len(wav_files)}")
+
+            for filename in wav_files:
+                filepath = os.path.join(emotion_dir, filename)
+                audio, sample_rate = librosa.load(filepath)
+                self.X.append(audio)
+                self.y.append(i)  # Use emotion index as label
+
+        self.y = np.array(self.y)  # Convert labels to numpy array
